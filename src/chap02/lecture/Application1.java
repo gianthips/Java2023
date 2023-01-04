0f09b3f170d0c8f1005bfa5bd9c515ce89260459
gianthips <gianthips@gmail.com>
2023-01-04 17:27:10 +0900

    230104

--- src/chap02/lecture/Application1.java
+++ src/chap02/lecture/Application1.java
@@ -57,6 +57,37 @@ public class Application1 {
 		int aa = (int)ch123;
 		System.out.println("영문자인지 확인:" + ((aa>=97 && aa<=122)||(aa>=65 && aa<=90)));
 		//형변환 안하고 그냥 ch123>='A' 또는 ch123 >= 97 이런식으로 해도 무방
+		
+		int num1 = 10;
+		
+		int result1 = (false && ++num1 >0)? num1: num1;
+		System.out.println("&&실행 확인:" + result1);
+		
+		/*삼항연산자*/
+		/*(조건식)? 참일 때 사용할 값: 거짓일 때 사용할 값; */
+		
+		int num1 = 10;
+		int num2 = -10;
+		
+		String result1 = (num1 > 0)? "양수다": "양수가 아니다.";
+		String result2 = (num2 > 0)? "양수다": "양수가 아니다.";
+		
+		System.out.println("num1은 " + result1);
+		System.out.println("num2는 " + result2);
+		
+		/* 중첩 사용 가능 */
+		int num3 = 5;
+		int num4 = 0;
+		int num5 = -5;
+		
+		String result3 = (num3>0)? "양수다.": (num3 == 0)? "0이다.": "음수다.";
+		String result4 = (num4>0)? "양수다.": (num4 == 0)? "0이다.": "음수다.";
+		String result5 = (num5>0)? "양수다.": (num5 == 0)? "0이다.": "음수다.";
+		
+		System.out.println("num3은" + result3);
+		System.out.println("num4는" + result4);
+		System.out.println("num5는" + result5);
+				
 	}
 
 }
