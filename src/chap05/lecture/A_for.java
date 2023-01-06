0cd82b5549a6131021e35466e8a90b1d20b71b01
gianthips <gianthips@gmail.com>
2023-01-06 10:16:00 +0900

    230106

--- src/chap05/lecture/A_for.java
+++ src/chap05/lecture/A_for.java
@@ -18,6 +18,11 @@ public class A_for {
 			//for(int i=1; ; i++) 이렇게 하면 계속 증가. true가 들어간거.
 			System.out.println(i);
 		}//빠져나올 때 i는 11이 된다.
+		
+		String str = "hello";
+		for(int i = 0; i<=str.length(); i++) {
+			System.out.println(str.charAt(i));
+		}
 				
 	}
 
