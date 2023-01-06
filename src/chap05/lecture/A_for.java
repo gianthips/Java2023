9e07bca6fd34ffdf0926f408b7297a8935f9a282
gianthips <gianthips@gmail.com>
2023-01-06 10:14:00 +0900

    230106

---
+++ src/chap05/lecture/A_for.java
@@ -0,0 +1,24 @@
+package chap05.lecture;
+
+public class A_for {
+
+	public void testSimpleForStatement() {
+
+		/*[for문 표현식]
+		 * for(초기식; 조건식; 증감식){}
+		 * 
+		 * 
+		 * */
+
+		/*
+		 * int i = 1; for(;i<=10;i++); 이렇게 해도 된다.
+		 */
+		
+		for(int i =1; i<=10; i++) {
+			//for(int i=1; ; i++) 이렇게 하면 계속 증가. true가 들어간거.
+			System.out.println(i);
+		}//빠져나올 때 i는 11이 된다.
+				
+	}
+
+}
---
+++ src/chap05/lecture/Application1.java
@@ -0,0 +1,10 @@
+package chap05.lecture;
+
+public class Application1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
