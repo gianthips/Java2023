e6a507a8d05507b91d617a2cf49451e284c0a65f
gianthips <gianthips@gmail.com>
2023-01-12 15:08:27 +0900

    230112

--- src/chap07_DTO/DTO/UserDTO.java
+++ src/chap07_DTO/DTO/UserDTO.java
@@ -1,5 +1,55 @@
 package chap07_DTO.DTO;
 
 public class UserDTO {
+	
+	private String id;
+	private String pwd;
+	private String name;
+	private java.util.Date enrollDate;
+	
+	public UserDTO() {}
+	
+	public UserDTO(String id, String pwd, String name, java.util.Date enrollDate) {
+		this.id = id;
+		this.pwd = pwd;
+		this.name = name;
+		this.enrollDate = enrollDate;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setEnrollDate(java.util.Date enrollDate) {
+		this.enrollDate = enrollDate;
+	}
+	
+	public String getID() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public java.util.Date getEnrollDate(){ // 이렇게 하는거구나
+		return enrollDate;
+	}
+	
+	public String getInformation() {
+		return "UserDTO [id=" + this.id 
+				+ ", pwd=" + this.pwd 
+				+ ", name=" + this.name 
+				+ ", enrollDate=" + this.enrollDate 
+				+ "]";
+	}
 
 }
