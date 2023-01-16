bb53b7d094dae357ad4d2105bd3c8c5870db3366
gianthips <gianthips@gmail.com>
2023-01-16 12:10:42 +0900

    230116

--- src/chap08_objectarray/section02/uses/MemberDTO.java
+++ src/chap08_objectarray/section02/uses/MemberDTO.java
@@ -1,6 +1,6 @@
 package chap08_objectarray.section02.uses;
 
-public class MemberDTO {
+public class MemberDTO { //extends java.lang.Object {
 	
 	private int num;
 	private String id;
@@ -10,7 +10,9 @@ public class MemberDTO {
 	private char gender;
 	
 	public MemberDTO() {}
+		
 	public MemberDTO(int num, String id, String pwd, String name, int age, char gender) {
+		super(); // 오브젝트의 생성자를 호출하는 것. extends 뒤에는 부모가 작성됨.
 		this.num = num;
 		this.id = id;
 		this.pwd = pwd;
@@ -18,51 +20,122 @@ public class MemberDTO {
 		this.age = age;
 		this.gender = gender;
 	}
+
 	
-	public void setNum(int num) {
-		this.num = num;
-	}
-	
-	public void setID(String id) {
-		this.id = id;
-	}
-	
-	public void setPWD(String pwd) {
-		this.pwd = pwd;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public void setGender(char gender) {
-		this.gender = gender;
-	}
+
+//	public MemberDTO(int num, String id, String pwd, String name, int age, char gender) {
+//		this.num = num;
+//		this.id = id;
+//		this.pwd = pwd;
+//		this.name = name;
+//		this.age = age;
+//		this.gender = gender;
+//	}
+//	
+//	public void setNum(int num) {
+//		this.num = num;
+//	}
+//	
+//	public void setID(String id) {
+//		this.id = id;
+//	}
+//	
+//	public void setPWD(String pwd) {
+//		this.pwd = pwd;
+//	}
+//	
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//	
+//	public void setGender(char gender) {
+//		this.gender = gender;
+//	}
+//	
+//	public int getNum() {
+//		return num;
+//	}
+//	
+//	public String getID() {
+//		return id;
+//	}
+//	
+//	public String getPwd() {
+//		return pwd;
+//	}
+//	
+//	public String getName() {
+//		return name;
+//	}
+//	
+//	public int getAge() {
+//		return age;
+//	}
+//	
+//	public char getGender() {
+//		return gender;
+//	}
 	
 	public int getNum() {
 		return num;
 	}
-	
-	public String getID() {
+
+
+	public String getId() {
 		return id;
 	}
-	
+
+
 	public String getPwd() {
 		return pwd;
 	}
-	
+
+
 	public String getName() {
 		return name;
 	}
-	
+
+
 	public int getAge() {
 		return age;
 	}
-	
+
+
 	public char getGender() {
 		return gender;
 	}
-	
+
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+
 	public String getInformation() {
 		return "MemberDTO [num=" + num +
 				", id=" + id +
--- src/chap08_objectarray/section02/uses/MemberManager.java
+++ src/chap08_objectarray/section02/uses/MemberManager.java
@@ -4,7 +4,16 @@ public class MemberManager {
 	
 	public void signUpMembers() {
 		
+		MemberDTO[] members = new MemberDTO[5];
 		
+		members[0] = new MemberDTO(1, "user01", "pass01", "홍길동", 20, '남');
+		members[1] = new MemberDTO(2, "user02", "pass02", "유관순", 16, '여');
+		members[2] = new MemberDTO(3, "user03", "pass03", "이순신", 40, '남');
+		members[3] = new MemberDTO(4, "user04", "pass04", "신사임당", 36, '여');
+		members[4] = new MemberDTO(5, "user05", "pass05", "윤봉길", 22, '남');
+		
+		MemberInsertManager memberInsertManager = new MemberInsertManager();
+		memberInsertManager.insert(members); // 여러개의 DTO값을 하나로 뭉쳐서 전달
 	}
 	
 	public void printAllMembers() {
